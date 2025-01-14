f8d3d6d84b7b6b213884b9deac5125a0ef5488cc
dd <dimych1601@gmail.com>
2025-01-14 13:02:25 +0300

    task_2

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -7,6 +7,16 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer, course, grade):
+        if (isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and
+                course in self.courses_in_progress):
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -14,6 +24,12 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+    grades = {}
+
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if (isinstance(student, Student) and course in self.courses_attached and
                 course in student.courses_in_progress):
@@ -23,10 +39,3 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-
-
-class Lecturer(Mentor):
-    pass
-
-class Reviewer(Mentor):
-    pass
